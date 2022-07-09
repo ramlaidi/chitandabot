f5be12c8f030b072ee3a99fab965e3958d3fcef0
ramlaidi <83389072+ramlaidi@users.noreply.github.com>
2022-07-09 15:02:13 +0700

    Update msg.js

--- message/msg.js
+++ message/msg.js
@@ -223,8 +223,8 @@ module.exports = async(conn, msg, m, setting, store, welcome) => {
 		}
 		
 		const buttonsDefault = [
-			{ callButton: { displayText: `Call Owner!`, phoneNumber: `+6285791458996` } },
-			{ urlButton: { displayText: `Script!`, url : `https://github.com/rtwone/chitandabot` } },
+			{ callButton: { displayText: `Call Owner!`, phoneNumber: `+6289691183687` } },
+			{ urlButton: { displayText: `Script!`, url : `https://github.com/ramlaidi/IrsanBotz` } },
 			{ quickReplyButton: { displayText: `🧑 Owner`, id: `${prefix}owner` } },
 			{ quickReplyButton: { displayText: `💰 Donasi`, id: `${prefix}donate` } }
 		]
@@ -325,7 +325,7 @@ module.exports = async(conn, msg, m, setting, store, welcome) => {
 		            break
 			case prefix+'donate':
 			case prefix+'donasi':
-			    reply(`──「 MENU DONATE 」──\n\nHi ${pushname} 👋🏻\n\`\`\`GOPAY : 085791458996\`\`\`\n\`\`\`PULSA : 085735338148 (Indosat)\`\`\`\nTerimakasih untuk kamu yang sudah donasi untuk perkembangan bot ini _^\n──「 THX FOR YOU ! 」──`)
+			    reply(`──「 MENU DONATE 」──\n\nHi ${pushname} 👋🏻\n\`\`\`GOPAY : 089691183687\`\`\`\n\`\`\`PULSA : 089691183687 (three 3)\`\`\`\nTerimakasih untuk kamu yang sudah donasi untuk perkembangan bot ini _^\n──「 THX FOR YOU ! 」──`)
 			    break
 			case prefix+'owner':
 			    for (let x of ownerNumber) {
@@ -450,7 +450,7 @@ module.exports = async(conn, msg, m, setting, store, welcome) => {
 				   caption: `${data.title}\n\nKamu bisa mengubahnya menjadi Vidio Tanpa Watermark atau Audio, pencet tombol dibawah untuk mengubahnya!`,
 				   buttons: [{buttonId: `${prefix}tiktoknowm ${args[1]}`, buttonText: { displayText: "Without Watermark" }, type: 1 },
 					{buttonId: `${prefix}tiktokaudio ${args[1]}`, buttonText: { displayText: "Audio" }, type: 1 }],
-				   footer: "Create by @irfann._x"
+				   footer: "Create by @irsan"
 			      }, { quoted: msg })
 				  limitAdd(sender, limit)
 			    }).catch(() => reply(mess.error.api))
